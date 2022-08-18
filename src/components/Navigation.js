2327938edc53acf08e1afaf36f20dfe7219f2aba
SynneFA <111527161+SynneFA@users.noreply.github.com>
2022-08-18 22:20:07 +0200

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
 import './Mainpage.css'
 
-
 function Navigation() {
-
-
   return (
     <div className="navBar">
     <h1 className="small-padding"> Dagbladet</h1> <h4 className="small-padding">PLUSS</h4><h4 className="small-padding" > E-AVIS</h4>
